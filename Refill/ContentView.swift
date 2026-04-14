9445e6d6fa8f5bead100edd67afcc6f0ffa3b610
jake <qiu5630@163.com>
2026-04-14 18:27:24 +0800

    Initial Commit

---
+++ Refill/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Refill
+//
+//  Created by Jake on 2026/4/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Refill/RefillApp.swift
@@ -0,0 +1,17 @@
+//
+//  RefillApp.swift
+//  Refill
+//
+//  Created by Jake on 2026/4/14.
+//
+
+import SwiftUI
+
+@main
+struct RefillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
