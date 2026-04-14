03aee61bacf54216fa209c033d203b8a0dcd6974
jake <qiu5630@163.com>
2026-04-14 22:31:11 +0800

    feat(widget): 添加 Refill 应用的 iOS Widget 扩展

---
+++ RefillWidget/RefillWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  RefillWidgetBundle.swift
+//  RefillWidget
+//
+//  Created by Jake on 2026/4/14.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct RefillWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        RefillWidget()
+    }
+}
